62434885880aed223e2bc738f32e1bf1592af8fa
WidloG <notatkiwidlo@gmail.com>
2023-06-14 16:01:27 +0200

    Initial commit

---
+++ src/test/java/com/example/springapp/SpringAppApplicationTests.java
@@ -0,0 +1,33 @@
+package com.example.springapp;
+
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SpringAppApplicationTests {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  void returnDefaultValue() throws Exception {
+    this.mockMvc.perform(get("/"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(content().string("Jenkin's pipeline"));
+  }
+
+}
